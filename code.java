45ecaaa2ba6a72121b08e6d9707b9731e0b76133
Sarwer Hussain <48062618+hussainint@users.noreply.github.com>
2019-09-29 15:22:47 +0530

    Create code.java

---
+++ code.java
@@ -0,0 +1,44 @@
+
+public class design
+{
+   
+    public static void main(String args[])
+    {
+         boolean bo;
+        design ob=new design();
+        while(true){
+            
+             int a=5,aa=1;
+            for(int ii=0;ii<4;ii++)
+            {
+                for(int kk=0;kk<aa;kk++)
+                System.out.println();
+                ++aa;
+                
+            System.out.println("\t\t        o o o o o o o o");
+            for( int jj=0;jj<a;jj++)
+            System.out.println("\t\t        o             o");
+            a=a-2;;
+            
+            System.out.println("\t\t        o o o o o o o o");
+            bo=ob.delay(); 
+            
+        }
+        System.out.println("\n\n\n\n\t\t        o o o o o o o o");
+        bo=ob.delay();  
+        }        
+    }
+    public boolean delay()
+   {      
+                      for(int j=0;j<100;j++)
+        {
+                         for(int i=0;i<=10000000;i++)
+                          for(int i1=0;i<=10000000;i++)
+                          for(int i2=0;i<=10000000;i++)
+                          for(int i3=0;i<=10000000;i++)
+                          for(int i4=0;i<=10000000;i++)
+                          for(int i5=0;i<=10000000;i++){}
+                         }  System.out.println('\u000c');
+            return true;
+    }
+}
